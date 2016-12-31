255ff987e387a8afec1b4028bf665a388f44d00f
JAYASELVAN  A  K <jayaselvanak@gmail.com>
2016-12-31 22:45:33 +0530

    Update strremvow.c

--- strremvow.c
+++ strremvow.c
@@ -2,6 +2,30 @@
 # include<string.h>
 int m ain()
 {
-
-return 0;
+    char s1[25];
+    char s2={'a','e','i','o','u','A','E','I','O','U',' '};
+    char s3[25];
+    int i,j,l,q=0;
+    scanf("%s",s1);
+    l=strlen(s1);
+    for(i=0;i<11;i++)
+    {
+        for(j=0;j<l;j++)
+        {
+            if(s1[j]==s2[i])
+            {
+                s1[j]='+';
+            }
+        }
+    }  
+    for(i=0;i<l;i++)
+    {
+        if(s1[i]!='+')
+        {
+            s3[q]=s1[i];
+            q++;
+        }
+    }
+    printf("%s",s3);
+    return 0;
 }
