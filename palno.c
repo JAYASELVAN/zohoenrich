f217eeca4e2a5ba78cba97afcab3b901f72caf51
JAYASELVAN  A  K <jayaselvanak@gmail.com>
2016-12-31 09:53:19 +0530

    Update and rename palindromenumber.c to palno.c

---
+++ palno.c
@@ -0,0 +1,16 @@
+# include<stdio.h>
+int main()
+{
+    int num,dup,tot=0,rem
+    printf("Enter the number");
+    scanf("%d",&num);
+    dup=num;
+    while(num!=0)
+    {
+        rem=num%10;
+        num=num/10;
+        tot=(tot*10)+rem;
+    }
+    (dup==tot)?printf("The number is palindrome"): printf("The number is not palindrome");
+    return 0;
+}
