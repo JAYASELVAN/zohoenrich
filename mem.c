b1a397a43c156db9457fd06b2069113814d2c74c
JAYASELVAN  A  K <jayaselvanak@gmail.com>
2017-01-07 14:31:00 +0530

    Update mem.c

--- mem.c
+++ mem.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 
 int main(void) {
-	int a[10]={1,2,3,4,5,6,7,8,9,10};
-	int i=0,t;
-	while(i<10)
-	{
-	    t=a[i];
-	    a[i]=a[(i+1)];
-	    a[(i+1)]=t;
-	    printf("%d\t%d\t",a[i],a[(i+1)]);
-	    i=i+2;
-	}
-	printf()
+	int x=65;
+	int y=0x31;
+	int z=0x3132;
+	int a=0x31324142;
+	int b=0x42413231;
+	int d=111222;
+	int ary[]={0x31324142,0x41423132,100};
+	printf("%d\n",sizeof(int));
+	printf("%c\n",sizeof(char));
+	printf("%d\n",a);
+	printf("%d\n",z);
+	printf("%d\n",b);
+	printf("%c\n",a);
+	printf("%c\n",b);
+	printf("%x\n",d);
+	printf("%s\n",ary);
 	return 0;
 }
 
