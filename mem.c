e21d9d8e0c08d5bad3e88a9c22a2f0348c81188c
JAYASELVAN  A  K <jayaselvanak@gmail.com>
2017-01-07 14:14:10 +0530

    Create mem.c

---
+++ mem.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	int a[10]={1,2,3,4,5,6,7,8,9,10};
+	int i=0,t;
+	while(i<10)
+	{
+	    t=a[i];
+	    a[i]=a[(i+1)];
+	    a[(i+1)]=t;
+	    printf("%d\t%d\t",a[i],a[(i+1)]);
+	    i=i+2;
+	}
+	printf()
+	return 0;
+}
+
