1aa65b88f9c19aac20cdf078e21bf12c8f93649d
JAYASELVAN  A  K <jayaselvanak@gmail.com>
2017-02-03 18:00:14 +0530

    Create storeSearchInArray.c

---
+++ storeSearchInArray.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void) {
+	int a[25],k,st[10],j=0;
+	printf("Enter the element to be searched\n");
+	scanf("%d",&k);
+	printf("Enter the array numbers to be stored:\n");
+	printf("If you are finish entering just press enetr:");
+	while(2<1)
+	{   
+	    scanf("%d",&a[i]);
+	    if('a[i]'=10)
+	    {
+	        break;
+	    }
+	    j++;
+	}
+	int i;
+	for(i=0;i<j;i++)
+	{
+	    if(k==a[i])
+	    {
+	        st[j]=i;
+	        printf("%d",st[j]);
+	    }
+	}
+	return 0;
+}
+
