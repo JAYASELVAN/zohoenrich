d1f7e937d7c9ffb216d3fa5398e4b7c39850d959
JAYASELVAN  A  K <jayaselvanak@gmail.com>
2017-06-14 18:47:40 +0530

    Create PrintCountryCapitalGDP.java

--- PrintCountryCapitalGDP.java
+++ PrintCountryCapitalGDP.java
@@ -4,12 +4,13 @@ public class Hello {
     public static void main(String[] args) 
     {
 		//Your Code Here
-    String s1,s2;
-    double num;
-    Scanner sc=new Scanner(System.in);
-    s1=sc.nextLine();
-    s2=sc.nextLine();
-    num=sc.nextDouble();
-    System.out.print("The capital of "+s1+" is"+s2+" and it's GDP is "+num);
+        String s1,s2;
+        double num;
+        Scanner sc=new Scanner(System.in);
+        s1=sc.nextLine();
+        s2=sc.nextLine();
+        num=sc.nextDouble();
+        num=Math.round (num*100.0)/100.0;
+        System.out.println("The capital of "+s1+" is "+s2+" and it's GDP is "+num);
 	}
 }
